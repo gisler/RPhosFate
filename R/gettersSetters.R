f648032ed929b831b00d2a2b5d38d455295479ef
gisler <gisler@users.noreply.github.com>
2021-05-29 18:09:20 +0200

    docs: add saveState and getLayer

--- R/gettersSetters.R
+++ R/gettersSetters.R
@@ -3,32 +3,53 @@ setGeneric(
   "getLayer",
   function(x, ...) standardGeneric("getLayer")
 )
+#' Get Layer
+#'
+#' Obtains a project raster layer for further analysis.
+#'
+#' @inheritParams erosionPrerequisites,RPhosFate-method
+#' @param i A character string specifying a layer name. Substance related layers
+#'   whose names start with _xx_ are treated differently. They have to be
+#'   queried by their name (not filename), for example, `"xxc"` together with
+#'   the respective substance specified in argument `j`, for example, `"PP"`,
+#'   which queries the particulate phosphorus concentration in top soil. See
+#'   subdirectory sections for further information.
+#' @param j A character string specifying a substance if applicable.
+#'
+#' @inheritSection catchment _Input_ subdirectory
+#'
+#' @inheritSection catchment _Intermediate_ subdirectory
+#'
+#' @inheritSection catchment _Result_ subdirectory
+#'
+#' @return A [`raster::RasterLayer-class`] object.
+#'
+#' @aliases getLayer
+#'
 #' @export
 setMethod(
   "getLayer",
   "RPhosFate",
-  function(x, layer, substance = NULL) {
-    qassert(layer, "S1")
+  function(x, i, j = NULL) {
+    qassert(i, "S1")
 
-    if (!is.null(substance)) {
-      assertSubstance(x, substance)
-      assertSubset(
-        layer,
-        sub("^rl_", "", slotNames(slot(x@substances, substance)))
-      )
+    if (!is.null(j)) {
+      assertSubstance(x, j)
+      assertSubset(i, sub("^rl_", "", slotNames(slot(x@substances, j))))
 
-      return(slot(slot(x@substances, substance), sprintf("rl_%s", layer)))
+      return(slot(slot(x@substances, j), sprintf("rl_%s", i)))
     } else {
       for (object in x@helpers@cv_rlo) {
-        if (layer %in% sub("^rl_", "", slotNames(slot(x, object)))) {
-          return(slot(slot(x, object), sprintf("rl_%s", layer)))
+        if (i %in% sub("^rl_", "", slotNames(slot(x, object)))) {
+          return(slot(slot(x, object), sprintf("rl_%s", i)))
         }
       }
     }
 
-    stop(sprintf("Layer %s was not found.", deparse(layer)), call. = FALSE)
+    stop(sprintf("Layer %s was not found.", deparse(i)), call. = FALSE)
   }
 )
+#' @rdname getLayer-RPhosFate-method
 #' @export
 setMethod(
   "[",
