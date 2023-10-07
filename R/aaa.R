f97e37cf7bcac7eb15afd0c55c95b50e2bc06258
gisler <gisler@users.noreply.github.com>
2023-10-07 19:43:17 +0200

    fix: import cor

--- R/aaa.R
+++ R/aaa.R
@@ -4,7 +4,7 @@
 #' @importFrom graphics abline clip par points
 #' @importFrom Rcpp sourceCpp
 #' @importFrom spatstat.geom as.owin as.ppp nncross
-#' @importFrom stats median optim optimize sd setNames
+#' @importFrom stats cor median optim optimize sd setNames
 #' @importFrom yaml read_yaml write_yaml
 #' @importFrom utils modifyList packageVersion
 #' @useDynLib RPhosFate, .registration = TRUE
