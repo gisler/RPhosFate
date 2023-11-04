7cb947c6e7ebe7a6d67447b5868cb6864bf1d399
gisler <gisler@users.noreply.github.com>
2023-11-04 17:04:23 +0100

    improve the improvements

--- src/D8slope.cpp
+++ src/D8slope.cpp
@@ -14,7 +14,7 @@ arma::dmat D8slope(
   /* With integers, missing values are stored as the smallest integer
    * (-2.147.483.648). See also https://adv-r.hadley.nz/rcpp.html
    */
-  int NA_integer_ = Rcpp::IntegerVector::get_na();
+  const int NA_integer_ = Rcpp::IntegerVector::get_na();
 
   arma::dmat nm_dem_pad(
     arma::size(nm_dem) + 2,
@@ -22,16 +22,15 @@ arma::dmat D8slope(
   );
   nm_dem_pad(1, 1, arma::size(nm_dem)) = nm_dem;
 
-  arma::uvec4 indices = {0, 2, 6, 8};
-  arma::ivec4 iv_fDo_dgl = {im_fDo.elem(indices)};
-  double ns_fpl_dgl = {std::sqrt(2.0 * std::pow(ns_fpl, 2.0))};
+  const arma::uvec4 indices = {0, 2, 6, 8};
+  const arma::ivec4 iv_fDo_dgl = {im_fDo.elem(indices)};
+  const double ns_fpl_dgl = {std::sqrt(2.0 * std::pow(ns_fpl, 2.0))};
 
   arma::dmat nm_slp(
     arma::size(nm_dem),
     arma::fill::value(Rcpp::NumericVector::get_na())
   );
 
-  double ns_tmp;
   #pragma omp parallel for num_threads(is_ths) collapse(2)
   for (arma::uword i = 1; i < nm_dem_pad.n_rows - 1; ++i) {
     for (arma::uword j = 1; j < nm_dem_pad.n_cols - 1; ++j) {
@@ -39,6 +38,7 @@ arma::dmat D8slope(
         continue;
       }
 
+      double ns_tmp;
       ns_tmp = nm_dem_pad.at(i, j) - arma::conv_to<double>::from(
         nm_dem_pad.submat(i - 1, j - 1, i + 1, j + 1).eval().elem(
           arma::find(im_dir.at(i - 1, j - 1) == im_fDo)
--- src/dir_sth.cpp
+++ src/dir_sth.cpp
@@ -13,15 +13,14 @@ arma::imat dir_sth(
   /* With integers, missing values are stored as the smallest integer
    * (-2.147.483.648). See also https://adv-r.hadley.nz/rcpp.html
    */
-  int NA_integer_ = Rcpp::IntegerVector::get_na();
+  const int NA_integer_ = Rcpp::IntegerVector::get_na();
 
   arma::imat im_sth_pad(arma::size(im_sth) + 2, arma::fill::value(NA_integer_));
   im_sth_pad(1, 1, arma::size(im_sth)) = im_sth;
 
-  int x = {1};
+  int is_sth = {1};
   arma::imat im_xxx(arma::size(im_sth), arma::fill::value(NA_integer_));
 
-  int tmp;
   #pragma omp parallel for num_threads(is_ths) collapse(2)
   for (arma::uword i = 1; i < im_sth_pad.n_rows - 1; ++i) {
     for (arma::uword j = 1; j < im_sth_pad.n_cols - 1; ++j) {
@@ -30,15 +29,16 @@ arma::imat dir_sth(
         continue;
       }
 
-      tmp = arma::conv_to<int>::from(
+      int is_tmp;
+      is_tmp = arma::conv_to<int>::from(
         im_sth_pad.submat(i - 1, j - 1, i + 1, j + 1).eval().elem(
           arma::find(im_dir.at(i - 1, j - 1) == im_fDo)
         )
       );
 
-      if (tmp != NA_integer_ && tmp != 0) {
-        im_xxx.at(i - 1, j - 1) = x;
-        ++x;
+      if (is_tmp != NA_integer_ && is_tmp != 0) {
+        im_xxx.at(i - 1, j - 1) = is_sth;
+        ++is_sth;
       }
     }
   }
