4d1c5b6915b58f209834d998368d136753a314e1
gisler <gisler@users.noreply.github.com>
2024-07-09 07:37:03 +0200

    refactor

--- src/MovingWindow.h
+++ src/MovingWindow.h
@@ -58,21 +58,21 @@ inline FacetRowsCols MovingWindow::determineFacetRowsCols(
   // Determine facet
   arma::uword us_fct{};
 
-  if        (ns_dir_inf <   90.0 && ns_dir_inf >=  45.0) {
+  if        (ns_dir_inf >=  45.0 && ns_dir_inf <   90.0) {
     us_fct = 0;
-  } else if (ns_dir_inf <   45.0 && ns_dir_inf >=   0.0) {
+  } else if (ns_dir_inf >=   0.0 && ns_dir_inf <   45.0) {
     us_fct = 1;
-  } else if (ns_dir_inf <= 360.0 && ns_dir_inf >= 315.0) {
+  } else if (ns_dir_inf >= 315.0 && ns_dir_inf <= 360.0) {
     us_fct = 2;
-  } else if (ns_dir_inf <  315.0 && ns_dir_inf >= 270.0) {
+  } else if (ns_dir_inf >= 270.0 && ns_dir_inf <  315.0) {
     us_fct = 3;
-  } else if (ns_dir_inf <  270.0 && ns_dir_inf >= 225.0) {
+  } else if (ns_dir_inf >= 225.0 && ns_dir_inf <  270.0) {
     us_fct = 4;
-  } else if (ns_dir_inf <  225.0 && ns_dir_inf >= 180.0) {
+  } else if (ns_dir_inf >= 180.0 && ns_dir_inf <  225.0) {
     us_fct = 5;
-  } else if (ns_dir_inf <  180.0 && ns_dir_inf >= 135.0) {
+  } else if (ns_dir_inf >= 135.0 && ns_dir_inf <  180.0) {
     us_fct = 6;
-  } else if (ns_dir_inf <  135.0 && ns_dir_inf >=  90.0) {
+  } else if (ns_dir_inf >=  90.0 && ns_dir_inf <  135.0) {
     us_fct = 7;
   } else {
     // The first two crash R (v4.4.0 with Rcpp v1.0.12), but would be better
